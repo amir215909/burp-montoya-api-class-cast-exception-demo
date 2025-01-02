b1d4159afbf2174e1d50d01d2db4d89023c92672
amir <ba97140@gmail.com>
2025-01-02 17:24:59 +0330

    Add montoya api and shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+    id("com.gradleup.shadow") version "8.3.3"
 }
 
 group = "demo.example"
@@ -11,6 +12,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    implementation("net.portswigger.burp.extensions:montoya-api:+")
 }
 
 tasks.test {
